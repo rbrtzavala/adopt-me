477d4345b001d7dd7f79252a6da67f8138545f15
Robert Zavala <rbrtzavala@gmail.com>
2024-01-10 12:04:55 -0700

    add react-query to project to fetchPet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
@@ -24,18 +25,27 @@ import Details from "./Details";
 //   ]);
 // };
 
+const queryClient = new QueryClient({
+  queries: {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  }
+})
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">
-          Adopt Me!
-        </Link>
-      </header>
-        <Routes>
-          <Route path="/details/:id"  element={<Details />} />
-          <Route path="/"  element={<SearchParams />} />
-        </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">
+            Adopt Me!
+          </Link>
+        </header>
+          <Routes>
+            <Route path="/details/:id"  element={<Details />} />
+            <Route path="/"  element={<SearchParams />} />
+          </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   )
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,38 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">
+          🐶
+        </h2>
+      </div>
+    )
+  }
+
+  const pet = results.data.pets[0];
+
   return (
-    <h2>{id}</h2>
+    <div className="details">
+      <h1>
+        {pet.name}
+      </h1>
+      <h2>
+      {`${pet.animal[0].toUpperCase() + pet.animal.slice(1)} - ${pet.breed} - ${pet.city}, ${pet.state}`}
+      </h2>
+      <button>
+        Adopt {pet.name}
+      </button>
+      <p>
+        {pet.description}
+      </p>
+    </div>
   );
 };
 
